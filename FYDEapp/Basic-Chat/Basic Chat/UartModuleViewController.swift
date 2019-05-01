a172ac20988ceee570e42096331caffe2896974a
arath36 <38861210+arath36@users.noreply.github.com>
2019-05-01 13:16:44 -0500

    Add files via upload

---
+++ FYDEapp/Basic-Chat/Basic Chat/UartModuleViewController.swift
@@ -0,0 +1,138 @@
+//
+//  UartModuleViewController.swift
+//  Basic Chat
+//
+//  Created by Trevor Beaton on 12/4/16.
+//  Copyright © 2016 Vanguard Logic LLC. All rights reserved.
+//
+
+
+
+
+
+import UIKit
+import CoreBluetooth
+
+class UartModuleViewController: UIViewController, CBPeripheralManagerDelegate {
+    
+    //UI
+
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    //Data
+    var peripheralManager: CBPeripheralManager?
+    var peripheral: CBPeripheral!
+    private var consoleAsciiText:NSAttributedString? = NSAttributedString(string: "")
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"Back", style:.plain, target:nil, action:nil)
+ 
+        //Create and start the peripheral manager
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        //-Notification for updating the text view with incoming text
+        updateIncomingData()
+    }
+    
+
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // peripheralManager?.stopAdvertising()
+        // self.peripheralManager = nil
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+        
+    }
+    
+    func updateIncomingData () {
+        NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: "Notify"), object: nil , queue: nil){
+            notification in
+            let appendString = "\n"
+            let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
+            let myAttributes2 = [NSFontAttributeName: myFont!, NSForegroundColorAttributeName: UIColor.red]
+            let attribString = NSAttributedString(string: "[Incoming]: " + (characteristicASCIIValue as String) + appendString, attributes: myAttributes2)
+            let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
+
+            
+            newAsciiText.append(attribString)
+            
+        }
+    }
+    
+    @IBAction func clickSendAction(_ sender: AnyObject) {
+        outgoingData()
+        
+    }
+    
+    
+    
+    func outgoingData () {
+        let appendString = "\n"
+        
+       // let inputText = inputTextField.text
+        
+        let myFont = UIFont(name: "Helvetica Neue", size: 15.0)
+        let myAttributes1 = [NSFontAttributeName: myFont!, NSForegroundColorAttributeName: UIColor.blue]
+        
+     //   writeValue(data: inputText!)
+        
+       // let attribString = NSAttributedString(string: "[Outgoing]: " + inputText! + appendString, attributes: myAttributes1)
+       // let newAsciiText = NSMutableAttributedString(attributedString: self.consoleAsciiText!)
+       // newAsciiText.append(attribString)
+        
+      //  consoleAsciiText = newAsciiText
+        //erase what's in the text field
+        
+    }
+    
+    // Write functions
+    func writeValue(data: String){
+        let valueString = (data as NSString).data(using: String.Encoding.utf8.rawValue)
+        //change the "data" to valueString
+        if let blePeripheral = blePeripheral{
+            if let txCharacteristic = txCharacteristic {
+                blePeripheral.writeValue(valueString!, for: txCharacteristic, type: CBCharacteristicWriteType.withResponse)
+            }
+        }
+    }
+    
+    func writeCharacteristic(val: Int8){
+        var val = val
+        let ns = NSData(bytes: &val, length: MemoryLayout<Int8>.size)
+        blePeripheral!.writeValue(ns as Data, for: txCharacteristic!, type: CBCharacteristicWriteType.withResponse)
+    }
+    
+    
+    
+
+    
+
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            return
+        }
+        print("Peripheral manager is running")
+    }
+    
+    //Check when someone subscribe to our characteristic, start sending the data
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        print("Device subscribe to characteristic")
+    }
+    
+
+    
+
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        if let error = error {
+            print("\(error)")
+            return
+        }
+    }
+}
+
